109d23045b2e4a71de138a78a9ec490940481fbf
Ororura <hitmanlol890@gmail.com >
2023-10-10 20:44:57 +0300

    bug fix

--- contracts/block2.sol
+++ contracts/block2.sol
@@ -1,28 +1,32 @@
 // SPDX-License-Identifier: MIT
+
+// Сделать функционал передачи оставшихся коинов; Сделать переводы оставшихся коинов на leftBalOwner.
 pragma solidity ^0.8.21;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
 contract CryptoMonster is ERC20("CryptoMonster", "CMON") {
-    uint public totalCoins = 10000000 * 10 ** 12;
+    uint public totalCoins = 10000000 * 10 ** decimals();
     uint public seedCoins = totalCoins * 10 / 100;
     uint public privateCoins = totalCoins * 30 / 100;
     uint public pubCoins = totalCoins * 60 / 100;
 
+    uint public leftBalOwner;
+    uint public publicPrice;
     uint public timeStart;
     uint public timeNow;
     uint public timeDif;
     uint public timeSystem;
     address public owner;
 
-    enum Role {Private, Provider}
+    enum Role {User, Private, Public}
 
     struct Ticket {
         string name;
         address usrAddress;
     }
 
-    // mapping(address => Ticket) requests;
+
     mapping(address => Role) public userRole;
     Ticket[] public requests;
     address[] public whiteList;
@@ -30,19 +34,21 @@ contract CryptoMonster is ERC20("CryptoMonster", "CMON") {
     //Shanghai
     constructor() {
         owner = msg.sender;
-        userRole[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = Role.Private;
-        userRole[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = Role.Provider;
         timeStart = block.timestamp;
         timeNow = timeStart;
-        timeDif = 0;
         timeSystem = timeStart;
+        timeDif = 0;
         _mint(msg.sender, totalCoins);
-        _transfer(msg.sender,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB , 300000 * 10 ** 12);
-        seedCoins = seedCoins - 300000 * 10 ** 12;
-        _transfer(msg.sender, 0x617F2E2fD72FD9D5503197092aC168c91465E7f2, 400000 * 10 ** 12);
-        seedCoins = seedCoins - 400000 * 10 ** 12;
-        _transfer(msg.sender, 0x17F6AD8Ef982297579C203069C1DbfFE4348c372, 200000 * 10 ** 12);
-        seedCoins = seedCoins - 200000 * 10 ** 12;
+
+        userRole[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = Role.Private;
+        userRole[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = Role.Public;
+
+        _transfer(msg.sender,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB , 300000 * 10 ** decimals());
+        seedCoins = seedCoins - 300000 * 10 ** decimals();
+        _transfer(msg.sender, 0x617F2E2fD72FD9D5503197092aC168c91465E7f2, 400000 * 10 ** decimals());
+        seedCoins = seedCoins - 400000 * 10 ** decimals();
+        _transfer(msg.sender, 0x17F6AD8Ef982297579C203069C1DbfFE4348c372, 200000 * 10 ** decimals());
+        seedCoins = seedCoins - 200000 * 10 ** decimals();
     }
 
     modifier AccesControl(Role _role) {
@@ -50,11 +56,47 @@ contract CryptoMonster is ERC20("CryptoMonster", "CMON") {
         _;
     }
 
+    modifier OnlyOwner() {
+        require(msg.sender == owner, unicode"Вы не владелец контракта");
+        _;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 12;
+    }
+
     function checkTime() public returns(uint){
         timeSystem = block.timestamp + timeDif;
-        uint dif = timeSystem - timeStart;
-        console.log(dif);
-        return dif;
+        uint timeCycle = (timeSystem - timeStart)/60;
+        console.log(timeCycle);
+        return timeCycle;
+    }
+
+    function updateLeftBalOwner () public {
+        if(checkTime() > 5) {
+            leftBalOwner += seedCoins;
+        }
+        if(checkTime() > 15) {
+            leftBalOwner += privateCoins;
+        }
+    }
+
+    function skipTime(uint _minutes) public {
+        timeDif += _minutes * 60;
+    }
+
+    function giveCoinsFromOwner(uint _amount, address _user) public OnlyOwner() {
+        require(leftBalOwner > 0, unicode"У вас нет остаточных коинов");
+        _transfer(msg.sender, _user, _amount * 10 **decimals());
+    }
+
+    function payReward(uint _amount, address _user) public AccesControl(Role.Public) {
+        _transfer(msg.sender, _user, _amount);
+        pubCoins -= _amount * 10 ** decimals();
+    }
+
+    function changePublicPrice(uint _newPrice) public AccesControl(Role.Public) {
+        publicPrice = _newPrice;
     }
 
     function makePrivateReq(string memory _name) public {
@@ -71,26 +113,30 @@ contract CryptoMonster is ERC20("CryptoMonster", "CMON") {
     }
 
     function buyPrivateToken(uint _amount) public payable returns(string memory) {
+        require(checkTime() >= 5, unicode"Приватная фаза ещё не началась");
+        require(checkTime() <= 15, unicode"Приватная фаза закончилась");
         require(_amount <= 100000, unicode"Больше 100000 нельзя купить");
         require(privateCoins > 0, unicode"Private коины закончились");
         for(uint i; i < whiteList.length; i++) {
             if(msg.sender == whiteList[i]) {
-                _transfer(owner, msg.sender, _amount * 10 ** 12);
-                privateCoins -= _amount * 10 ** 12;
+                _transfer(owner, msg.sender, _amount * 10 ** decimals());
+                privateCoins -= _amount * 10 ** decimals();
                 payable(msg.sender).transfer(_amount * 750000000000000);
             }
             else {
-                return unicode"Вас нет в WhiteList";
+                return unicode"Free sale not started";
             }
         }
+
         return unicode"Операция проведена успешна";
     }
 
     function buyPublicToken(uint _amount) public payable {
+        require(checkTime() >= 10, unicode"Public фаза ещё не началась");
         require(_amount <= 5000, unicode"Больше 5000 нельзя купить");
         require(pubCoins > 0, unicode"Public коины закончились");
-        _transfer(owner, msg.sender, _amount * 10 ** 12);
-        pubCoins -= _amount * 10 ** 12;
+        _transfer(owner, msg.sender, _amount * 10 ** decimals());
+        pubCoins -= _amount * 10 ** decimals();
         payable(msg.sender).transfer(_amount * 1000000000000000);
     }
 
